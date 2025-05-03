f5cc99bfd4378f34db88443541b1b00a935b0e9a
locnht <locnht.it@gmail.com>
2025-05-03 18:06:02 +0700

    fix jquery

--- src/components/client/documents/digital.signature/usb.digital.signature.jsx
+++ src/components/client/documents/digital.signature/usb.digital.signature.jsx
@@ -1,8 +1,8 @@
 import React, { useState, useEffect, useCallback, useRef } from "react";
 import { App, Modal, Spin } from "antd";
 import { useNavigate } from "react-router-dom";
-import "Scripts/jquery-3.4.1.min.js";
-import "Scripts/jquery.signalR-2.4.1.min.js";
+import "/Scripts/jquery-3.4.1.min.js";
+import "/Scripts/jquery.signalR-2.4.1.min.js";
 import { createHandleTaskActionAPI } from "@/services/api.service";
 import { useCurrentApp } from "@/components/context/app.context";
 
